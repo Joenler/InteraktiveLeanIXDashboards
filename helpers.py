161b03a5b673568e57b87856d2ebd9b8cda1337d
jonathanjonler <jjanle17@student.aau.dk>
2021-03-12 13:22:24 +0100

    get_auth method added

---
+++ helpers.py
@@ -0,0 +1,10 @@
+import json
+
+
+def get_auth(config_file : str) -> tuple:
+    with open(config_file) as f:
+        data = json.load(f)
+        return (data["api_token"], data["auth_url"], data["request_url"])
+
+
+secrets = get_auth('AUTH.json')
