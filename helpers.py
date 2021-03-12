cf6edfb8256398cd60a70e46451062fc76c86ec7
jonathanjonler <jjanle17@student.aau.dk>
2021-03-12 14:11:17 +0100

    Added query function. Need somewhere to store the data now.

--- helpers.py
+++ helpers.py
@@ -1,5 +1,5 @@
 import json
-
+import requests
 
 def get_auth(config_file : str) -> tuple:
     with open(config_file) as f:
@@ -9,3 +9,17 @@ def get_auth(config_file : str) -> tuple:
 
 secrets = get_auth('authentification.json')
 
+def query_leanix(query: str, auth: str):
+    authentication = get_auth(auth)
+    response = requests.post(authentication[1],
+                             auth=('apitoken', authentication[0]),
+                             data={'grant_type': 'client_credentials'})
+    response.raise_for_status()
+    access_token = response.json()['access_token']
+
+    data = {"query": query}
+    json_data = json.dumps(data)
+    auth_header = 'Bearer ' + access_token
+    header = {'Authorization': auth_header}
+
+    return requests.post(url=authentication[2], headers=header, data=json_data)
--- main.py
+++ main.py
@@ -1,44 +1,24 @@
-import json
-import requests
 import pandas as pd
-import datetime
 from helpers import *
-auth_url, api_token, request_url = get_auth("authentification.json")
 
-response = requests.post(auth_url,
-                         auth=('apitoken', api_token),
-                         data={'grant_type': 'client_credentials'})
-response.raise_for_status()
-access_token = response.json()['access_token']
-graphql_query = """{
-  allFactSheets(factSheetType: Application) {
-    edges {
-      node {
-        name
-        completion {
-          completion
-          percentage
-                    }  
+
+
+
+df = pd.json_normalize(query_leanix( """{
+      allFactSheets(factSheetType: Application) {
+        edges {
+          node {
+            name
+            completion {
+              completion
+              percentage
+                        }
+            }
+          }
         }
-      }
-    }
-}"""
-
-data = {"query": graphql_query}
-json_data = json.dumps(data)
-auth_header = 'Bearer ' + access_token
-header = {'Authorization': auth_header}
-
-response = requests.post(url=request_url, headers=header, data=json_data)
-response.raise_for_status()
-df = pd.json_normalize(response.json()['data']['allFactSheets']['edges'])
+    }""", "authentification.json").json()['data']['allFactSheets']['edges'])
 df = df[["node.name", "node.completion.percentage"]]
 below25 = df[df["node.completion.percentage"] < 25]
 below50above25 = df[(df["node.completion.percentage"] < 50) & (df["node.completion.percentage"] > 25)]
 above50 = df[df["node.completion.percentage"] > 50]
 description = df.describe()
-with open("data.csv", 'a') as f:
-    f.write(f"Dato: {datetime.date.today()} \n")
-    f.write(f"Gennemsnitlig completion procent: {description.iloc[1, :].values[0].round(2)}% \n")
-
-# df.to_excel('my_graphql_data.xlsx', index=False)
