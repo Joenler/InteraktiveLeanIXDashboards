e54fbec2f291e9d267475db03dd568dc83bda659
jonathanjonler <jjanle17@student.aau.dk>
2021-03-13 21:30:06 +0100

    Trying out the flask library.

--- main.py
+++ main.py
@@ -16,9 +16,21 @@ query = """{
     }"""
 
 
-df = response_to_df(query_leanix(query, "authentification.json"))
-df = df[["node.name", "node.completion.percentage"]]
-below25 = df[df["node.completion.percentage"] < 25]
-below50above25 = df[(df["node.completion.percentage"] < 50) & (df["node.completion.percentage"] > 25)]
-above50 = df[df["node.completion.percentage"] > 50]
-description = df.describe()
+# df = response_to_df(query_leanix(query, "authentification.json"))
+# df = df[["node.name", "node.completion.percentage"]]
+# below25 = df[df["node.completion.percentage"] < 25]
+# below50above25 = df[(df["node.completion.percentage"] < 50) & (df["node.completion.percentage"] > 25)]
+# above50 = df[df["node.completion.percentage"] > 50]
+# description = df.describe()
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "This is the main page!"
+
+if __name__ == "__main__":
+    app.run()
