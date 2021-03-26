4d9a358963dc3591419ba11643a6416fde5a511b
jonathanjonler <jjanle17@student.aau.dk>
2021-03-26 13:04:00 +0100

    Taking the first steps towards the system owners figures

--- completion_dashboard.py
+++ completion_dashboard.py
@@ -1,9 +1,6 @@
 import pandas as pd
-import plotly
 import plotly.express as px
-import plotly.graph_objects as go
 from datetime import date
-import re
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -15,6 +12,7 @@ from dash.dependencies import Input, Output
 
 app = dash.Dash(__name__)
 
+# This is going to be replaced with some database call
 df = pd.read_csv("completions.csv", names=["Date", "Completion"])
 
 ######################
@@ -26,18 +24,18 @@ app.layout = html.Div([
 
     dcc.DatePickerRange(
         id="choose-period",
-        min_date_allowed=date(2021, 3, 19),
-        max_date_allowed=date(2021, 4, 24),
-        initial_visible_month=date(2021, 3, 19),
-        start_date=date(2021, 3, 19),
-        end_date=date(2021, 4, 24),
-        style={"width": "500px", 'text-align': 'center'}
+        min_date_allowed=date(2021, 3, 19),  # Here we will have the earliest date in the database.
+        max_date_allowed=date(2021, 4, 24),  # Here we will have the latest date in the database.
+        initial_visible_month=date(2021, 3, 19),  # Here we will have the current date
+        start_date=date(2021, 3, 19),  # Here we will have the earliest date in the database.
+        end_date=date(2021, 4, 24),  # Here we will have the latest date in the database.
+        style={"width": "648px", 'text-align': 'center'}
 
     ),
 
     dcc.Graph(id='completions', figure={})
 
-], style={'width': "500px", 'height': "500px"})
+], style={'width': "648px", 'height': "384px"})
 
 ######################
 # Dashboard Callback #
@@ -49,19 +47,16 @@ app.layout = html.Div([
      Input("choose-period", "end_date")])
 def update_output(start_date, end_date):
 
-    dff = df.copy()
+    dff = df.copy()  # In the future we will fetch the relevant data from a database instead.
     dff = dff[(dff['Date'] > start_date) & (dff['Date'] <= end_date)]
     fig = px.line(
         x=dff["Date"],
         y=dff["Completion"],
-        labels={"x": "Date", "y": "Completion"}
+        labels={"x": "Date", "y": "Completion in %"}
 
         )
 
-
     return fig
-
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
